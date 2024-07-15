cf54992bd012eb58a1219812de90fd3fa7936554
kk7188048 <kk7188048@g]mail.com>
2024-07-15 11:11:52 +0530

    fd

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,7 +3,7 @@ import {Navigate} from 'react-router-dom'
 import {UserContext} from "./UserContext";
 import {useContext} from "react";
 
-axios.defaults.baseURL = 'https://airbnb-npkz6olnt-kk7188048s-projects.vercel.app/';
+axios.defaults.baseURL = `${window.location.origin}`;
 axios.defaults.withCredentials = true;
 import {Route, Routes} from "react-router-dom";
 import IndexPage from "./pages/IndexPage.jsx";
